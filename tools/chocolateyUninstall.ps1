4a276aeef2104d56c854de10d203ca97e44c6aa8
adriens <Adrien.Sales@gmail.com>
2021-01-01 18:56:03 +1100

    Apply chocolatey guidelines

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$bat = Join-Path $content 'imgpkg'
+
+Uninstall-BinFile `
+  -Name 'imgpkg' `
+  -Path $bat
